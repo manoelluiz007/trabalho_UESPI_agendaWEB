c9aea297f3fcc2f872e6c0c9d433933d91ef18af
manoelluiz007 <manoelluiz007@gmail.com>
2015-09-13 10:56:13 -0300

    Create footer.inc.php

---
+++ Inc/footer.inc.php
@@ -0,0 +1,17 @@
+<?php
+	/* Terceira Avaliacao da Disciplina Programacao 3 WEB
+	 * Desenvolvido por
+	 *		Manoel Luiz de Carvalho Melo
+	 */
+echo "
+<!-- Script 3.3 - footer.html -->
+	<!-- End of the page-specific content. --></div>
+	
+	<div id='footer'>
+		<p>Copyright &copy; <a href='#'>Plain and Simple</a> 2007 | Designed by <a href='http://www.edg3.co.uk/'>edg3.co.uk</a> | Sponsored by <a href='http://www.opendesigns.org/'>Open Designs</a></p>
+		<p>Valid <a href='http://jigsaw.w3.org/css-validator/'>CSS</a> &amp; <a href='http://validator.w3.org/'>XHTML</a> &amp; <a href=''>PHP</a></p>
+		<p>Developer by Manoel Luiz C.M</p>
+	</div>
+</body>
+</html>
+";
